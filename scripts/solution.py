6d0b1a953cef304a011182f5f0f55584647afb63
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-06-08 19:45:20 -0500

    added message_generation, added a .msg file to subscribe, rewrote solution.py

--- scripts/solution.py
+++ scripts/solution.py
@@ -1,29 +1,40 @@
 #!usr/bin/env python
 import rospy
-from std_msgs.msg import String
-from TwoInts.msg import 
+#type of message to publish
+from std_msgs.msg import Int16
+#message to subscribe
+from project1.msg import TwoInts
 
-def publisherRospy():
-	publisher1 = rospy.Publisher('two_ints', String,queue_Size=0)
-	rospy.init_node('publisher', anonymous=True)
+result=0;
 
-	sleepness = rospy.Rate(1)
+def callback(msg):
+	num1 = msg.a
+	num2 = msg.b
+	result = num1 + num2
 
-	while not rospy.is_shutdown():
-		publisher1.publish()
-		sleepness.sleep()
+	#now lets publish
+	resultFromSum = Int16()
+	resultFromSum.data = result
+	publisher.publish
 
-#For the subscriber we need a callback
-def rospyCallback():
-	pass
-def subscriberRospy():
-	rospy.init_node('subscriber',String, callback)
-	rospy.Subscriber('two_ints')
+def main():
+	#initialize the node
+	rospy.init_node('two_ints_publisher_node')
+
+	#create a publisher
+	#the topic, the message type, queue
+	publisher = rospy.Publisher('sum', Int16, queue_size=0)
+
+	#create a subscriber
+	#topic
+	subscriber = rospy.Subscriber('two_ints', TwoInts, callback)
+
+	#let it rip
 	rospy.spin()
 
 if __name__ == '__main__':
 	try:
-		subscriberRospy()
+		main()
 
 	except Exception as e:
 		pass
